aa851fb5df6cb70b1dddd884e13052beb6a207ae
Serhii Antonets <antonets.serhii.ua@gmail.com>
2024-04-18 00:42:54 +0300

    Update SearchBox.jsx

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,24 +1,30 @@
-import clsx from "clsx";
-import css from "./SearchBox.module.css"
+import css from "./SearchBox.module.css";
 import { useId } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { changeFilter } from "../../redux/filtersSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
 
-
-  const SearchBox = () => { 
+const SearchBox = () => {
   const dispatch = useDispatch();
   const searchID = useId();
-    
-  const handleChange = (evt) => {
-    dispatch(changeFilter( evt.target.value ));
-  } 
+  const nameFilter = useSelector(selectNameFilter);
 
-   
+  const handleChange = (evt) => {
+    dispatch(changeFilter(evt.target.value));
+  };
 
   return (
     <div>
-      <label className={css.searchLabel} htmlFor={searchID}>Find contacts by name</label>
-      <input className={css.searchInput} id={searchID} type="text"  onChange={handleChange}/>
+      <label className={css.searchLabel} htmlFor={searchID}>
+        Find contacts by name
+      </label>
+      <input
+        className={css.searchInput}
+        id={searchID}
+        type="text"
+        value={nameFilter}
+        onChange={handleChange}
+      />
     </div>
   );
 };
